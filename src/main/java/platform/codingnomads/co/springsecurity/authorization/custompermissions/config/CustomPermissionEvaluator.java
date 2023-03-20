c63f2e603cd5ab3417399038e5e36dc9ec1878c1
utstudent <talhakaran@utexas.edu>
2023-03-19 23:51:52 -0500

    Spring Security

--- src/main/java/platform/codingnomads/co/springsecurity/authorization/custompermissions/config/CustomPermissionEvaluator.java
+++ src/main/java/platform/codingnomads/co/springsecurity/authorization/custompermissions/config/CustomPermissionEvaluator.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Component
 public class CustomPermissionEvaluator implements PermissionEvaluator {
+    
     // better suited for @PostAuthorize as we have access to the return object
     @Override
     public boolean hasPermission(Authentication authentication, Object targetDomainObject, Object permission) {
